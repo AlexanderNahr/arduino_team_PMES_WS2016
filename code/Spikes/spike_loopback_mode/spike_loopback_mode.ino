4e3d69b11fa2462943e1998afbfa44ce39e8cc6e
AlexanderNahr <alexnahr@gmail.com>
2017-01-19 21:24:55 -0600

    Update spike for loopback
    added 'lazy' loopback function for one char loopback because whole words
    wasn't working

--- code/Spikes/spike_loopback_mode/spike_loopback_mode.ino
+++ code/Spikes/spike_loopback_mode/spike_loopback_mode.ino
@@ -13,6 +13,7 @@
 
 /******************************************************************************************************************/
 // function declaration
+void lazy_loop_back_mode();
 void loop_back_mode();
 void bidirectional_mode();
 
@@ -20,6 +21,7 @@ void bidirectional_mode();
 // constants
 #define LOOPBACK      0
 #define BIDIRECTIONAL 1
+#define LAZY_LOOPBACK 2
 
 /******************************************************************************************************************/
 // globals
@@ -43,13 +45,16 @@ void setup()
   // Set data rate for the SW SoftwareSerial port
   mySerial.begin(9600);
 
-  g_mode_of_operation = LOOPBACK; // BIDIRECTIONAL; // set mode of operation
+  g_mode_of_operation = LAZY_LOOPBACK; //LOOPBACK; // BIDIRECTIONAL; // set mode of operation
 }
 
 void loop() 
 {
   switch( g_mode_of_operation )
   {
+    case LAZY_LOOPBACK:
+      lazy_loop_back_mode();    // sends a received character back immediately with carriage return
+      break;
     case LOOPBACK:              // send received message back immediately
       loop_back_mode();
     break;
@@ -62,30 +67,82 @@ void loop()
 }
 
 /****************************************************************************************************************//*
-   \brief receive message from Wifly module and send it right back
+   \brief receive character from Wifly module and send it right back
+   \author Alexander
+   \date Jan 19, 2017
+   // Problems are:
+   // 1. only characters, not packages/words are send back (-> check for \r\n)
+   // 2. 255 value (weird y value in ASCII table) shows up when more than 1 char is send from Wifly module
+
+/******************************************************************************************************************/
+void lazy_loop_back_mode()
+{
+  char char_received = -1;
+  if( mySerial.available() > 0 ) 
+  {
+    // read from Sw serial (Wifly) and print to HW Serial (Arduino serial monitor)
+    char_received = mySerial.read();
+    Serial.println( char_received );
+
+    // send received character right back where it came from..Sw Serial (Wifly)
+    mySerial.println( char_received );
+    
+  }
+  
+}
+/****************************************************************************************************************//*
+   \brief receive message (as a string) from Wifly module and send it right back
    \author Alexander
    \date Jan 18, 2017
+
+   Well, this one isn't working as I planned it to. Seems to be in an eternal loop.
 /******************************************************************************************************************/
 void loop_back_mode()
 {
   char char_to_send_back = 'A';
-  char string_to_send[ 100 ] = "MyAnswer";
+  char string_to_send[100] = "";
+  char char_received = -1;
+  
 
   // read from SW Serial (Wifly Board) and write to HW Serial (Arduino Serial Monitor)
-  if (mySerial.available()) 
+  if( mySerial.available() > 0 ) 
   {
-    char_to_send_back = Serial.write(mySerial.read());
-  }
+    int i = 0;
+    // read from Sw serial (Wifly) and print to HW Serial (Arduino serial monitor)
+    char_received = mySerial.read();
+
+    // loop through received string until no character in queue or '\0' reached
+    while( char_received != -1 && char_received != '\0' )
+    {
+      
+      Serial.println( char_received );            // print to Arduino Serial Monitor
   
+      char_received = mySerial.read();            // read 1 char from Wifly queue
+      string_to_send[i] = char_received;          // add to string
+      if( char_received = '\0' )                  // end of received string reached
+      {
+        mySerial.println( string_to_send );       // send whole string back to sender (Wifly module)
+      }
+      
+      i++;                                        // increment counter for string
+    }
+  }
+
   // read from HW serial (Arduino Serial Monitor) and write to SW serial (Wifly Board)
-  if (Serial.available()) //--> PROBLEM HERE, need to flush queue here
+  /*if (Serial.available() > 0) //--> PROBLEM HERE, need to flush queue here
   {
     // debug code for Serial Monitor
     Serial.print( "Following String will be send to Wifly Modul" );
-    Serial.print( string_to_send );
-    
-    mySerial.write( string_to_send );  // send this to Wifly modul, should appear in Telnet Com Window
-  }
+    //Serial.print( );//string_to_send );
+    Serial.println();
+    int read = Serial.read();
+    mySerial.print( string_to_send );
+    //mySerial.print( "print" );
+    //mySerial.println(string_to_send); // is NOT send
+    mySerial.println();// "println");     // is send twice
+    //mySerial.write( "write\n\r" );  // send this to Wifly modul, should appear in Telnet Com Window
+  }*/
+  //delay(100); // delay hat nichts gebracht..
 }
 
 /****************************************************************************************************************//*
