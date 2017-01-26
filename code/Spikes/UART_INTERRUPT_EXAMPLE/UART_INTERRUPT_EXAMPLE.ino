d997b5a31446edf607fa92cf2a2747f505ee74f2
Florian <florian.bartsch@online.de>
2017-01-26 18:53:51 +0100

    Ich habe mal versucht die Module etwas zu kapseln. Was haltet ihr davon? Eigentlich wollte ich die externen Variablen mit einem HEader einbinden. Das hat aber nicht geklappt wie ich mir das vorgestellt habe.
    Ich habe dann versucht eine Sturktur mit den 3 Globalen Variaben zu schnüren.
    Auch das hat nicht funktioniert. Arduino ist und bleibt komisch :-)

--- code/Spikes/UART_INTERRUPT_EXAMPLE/UART_INTERRUPT_EXAMPLE.ino
+++ code/Spikes/UART_INTERRUPT_EXAMPLE/UART_INTERRUPT_EXAMPLE.ino
@@ -2,90 +2,36 @@
  * @brief Interrupt based UART Example
  * @author F.Bartsch
  */
-
 #include <SoftwareSerial.h>
-
-// Create SoftwareSerial object
-SoftwareSerial mySerial(10, 11); // RX, TX
-
-// set up glabal data 
-static String rxString = "";
-
-/**
- * @brief Interface to get rxString in other Modules
- * @details Use Funktion to  work with the Data in other modules. 
- */
-String GetRxString(void){
-  return (rxString);
-}
-
-// this flag indicates a received sting is ready to work with.
-// note that that is the only global data structure in this module
-boolean rxStringCompleteFlag = false;
-boolean rxStringOverflowFlag = false;
 
 void setup()
 {
-  // Set data rate for hw serial port on Arduino board Pin 0 and Pin 1
-  Serial.begin(9600);
-
-  while (!Serial) {
-    ; // wait for serial port to connect.
-  }
-  
-  // Set data rate for the sw serial port
-  mySerial.begin(9600);
-
-  while (!mySerial) {
-    ; // wait for sw serial port to connect.
-  }
-
-  rxString.reserve(200);
+  UartInit();
 }
+
+extern SoftwareSerial SW_Serial;
+extern String rxString;
+extern boolean rxStringOverflowFlag;
+extern boolean rxStringCompleteFlag;
 
 void loop() {
   
   // send something to wifi module if a complete string was received
   if (rxStringCompleteFlag) 
   {
-    mySerial.write("STRING RECEIVED");
+    // DO SOMETHING WITH THE RECEIVED STRING!
+    // CALL PROTOKOLL HERE
+    
+    SW_Serial.write("STRING RECEIVED");
     
     // clear the string:
-    rxString = "";
+    rxString= "";
     rxStringCompleteFlag = false;
   }
   
   if (rxStringOverflowFlag)
   {
-    mySerial.write("STRING TOO LONG!");
-  }
-}
-
-/**
- * @brief rx-isr of hw uart
- */
-void serialEvent() {
-  
-  while (Serial.available()) 
-  {
-    // catch the incoming char
-    char inChar = (char)Serial.read();
-    
-    // add it to the inputString:
-    rxString += inChar;
-
-    if (rxString.length() >= 200)
-    {
-      rxString = ""
-      rxStringOverflowFlag = true;
-    }
-    
-    // set flag if incoming char is the termination sign "]".
-    // this signals to the main loop to do somthing with the data
-    if (inChar == ']')
-    {
-      rxStringCompleteFlag = true;
-    }
+    SW_Serial.write("STRING TOO LONG!");
   }
 }
 
