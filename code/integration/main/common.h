aa3a3022d1351ad83f74bf10b41cefdabd496c47
AlexanderNahr <alexnahr@gmail.com>
2017-02-24 12:18:03 -0600

    Create #defines for state machine
    This is a proposal for state machine values. State machine will start
    AFTER a string is received and interpreted by 'Parser' module. Parser
    will need to use the same values as in the #defines committed here.

--- code/integration/main/common.h
+++ code/integration/main/common.h
@@ -2,7 +2,7 @@
    \file    common.h
    \brief   contains common declarations throughout this project
    \author  Alexander Nahrwold
-   \date    2.2.2017
+   \date    24.2.2017
    \todo    
 *******************************************************************************************************************/
 
@@ -13,6 +13,20 @@
 /******************************************************************************************************************/
 // include files
 #include "Arduino.h"
+
+/******************************************************************************************************************/
+// definitions 
+
+// STATEMACHINE
+#define ERROR            -1  //!< wifi protocol identiefier not recognized
+#define LOGIN_SUCCESSFUL  0  //!< login to wifly module successful (pw corrct)
+#define LOGIN_PW_WRONG    1  //!< login to wifly module failed  (pw wront)
+#define LOGOUT_SUCCESSFUL 2  //!< logout from wifly module initiated (pw correct)
+#define LOGOUT_PW_WRONG   3  //!< logout from wifly modle failed (pw incorrect)
+#define ORDER_SUCCESSFUL  4  //!< order received, order string correct
+#define ORDER_WRONG       5  //!< order received, order string incorrect
+#define ORDER_PW_WRONG    6  //!< order received, pw incorrect
+#define BROADCAST         7  //!< broadcast enabled
 
 /******************************************************************************************************************/
 // globals
